dae2ef63c1ba86ab60b19411c8e17747af42e1e7
wf <dorofeev_av@tgt72.ru>
2018-07-04 14:35:48 +0500

    tmux tpm install plugin command
    thanks to https://github.com/tmux-plugins/tpm/issues/6#issuecomment-54520698

--- zsh-bootstrap.sh
+++ zsh-bootstrap.sh
@@ -108,6 +108,15 @@ mkdir -p ~/.tmux/plugins/
 if [ ! -d ~/.tmux/plugins/tpm ]; then
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required, I guess
+tmux kill-server
+
 
 #import bash history to zsh
 if which ruby; then
