2a931bcc0f42ee82fb960054717e600652d24caf
wf <dorofeev_av@tgt72.ru>
2018-07-04 13:34:24 +0500

    added support for freebsd

--- zsh-bootstrap.sh
+++ zsh-bootstrap.sh
@@ -5,8 +5,29 @@ BASEDIR=$(dirname "$SCRIPT")
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 
 # install zsh
-sudo apt update
-sudo apt -y install zsh wget git tmux
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" == "linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+# For everything else (or if above failed), just use generic identifier
+[ "$DISTRO" == "" ] && export DISTRO=$UNAME
+unset UNAME
+
+if [ "$DISTRO" == "Ubuntu" ]; then
+	sudo apt update
+	sudo apt -y install zsh wget git tmux
+fi
+
+if [ "$DISTRO" == "freebsd" ]; then
+	sudo pkg install zsh wget git tmux
+fi
 
 cd $HOME
 
