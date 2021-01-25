5885fbc407bc5f25a91fa88c2ff5f593c16920f6
dorofeevav <dorofeevav@ecoteo.ru>
2021-01-25 16:54:38 +0500

    tmux config update fixed

--- zsh-update-configs.sh
+++ zsh-update-configs.sh
@@ -35,11 +35,6 @@ if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
 fi
 cat $BASEDIR/zshrc | sed "s,HOME_DIR,$HOME," > ~/.zshrc
 
-if [ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ]; then
-	mv ~/.tmux.conf ~/.tmux.conf.bak;
-fi
-cp $BASEDIR/tmux.conf ~/.tmux.conf
-
 # vimrc install
 if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
 	mv ~/.vimrc ~/.vimrc.bak;
@@ -47,7 +42,20 @@ fi
 
 cp $BASEDIR/.vimrc ~/.vimrc
 
+# TMUX configure
 currentver="$(tmux -V | cut -f2 -d" ")"
+requiredver="2.9"
+ if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
+    if [ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ]; then
+      mv ~/.tmux.conf ~/.tmux.conf.bak;
+    fi
+    cp $BASEDIR/tmux_29.conf ~/.tmux.conf
+ else
+    if [ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ]; then
+      mv ~/.tmux.conf ~/.tmux.conf.bak;
+    fi
+    cp $BASEDIR/tmux.conf ~/.tmux.conf
+ fi
 requiredver="2.1"
  if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
         echo "set -g mouse on" >> ~/.tmux.conf
@@ -57,9 +65,6 @@ requiredver="2.1"
         echo "set -g mouse-resize-pane on" >> ~/.tmux.conf
         echo "set -g mouse-select-window on" >> ~/.tmux.conf
  fi
-unset currentver
-unset requiredver
-echo "run '~/.tmux/plugins/tpm/tpm'" >> ~/.tmux.conf
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
 	cd ~/.tmux/plugins/tpm
